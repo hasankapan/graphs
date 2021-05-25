46b7276435b93cd5897ce38f896355f7cd5b0cb6
hasan kapan <hasankapan36@gmail.com>
2021-05-25 17:14:24 +0300

    -

--- src/lib/graphs.component.ts
+++ src/lib/graphs.component.ts
@@ -1,23 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { NameValue } from './models/inputs/data/core/NameValue.model';
 
 @Component({
   selector: 'ngss-graphs',
-  template: `
-    <ngss-pie-graph *ngIf="nameValueList.length > 0" [mainDataSource]='nameValueList'></ngss-pie-graph>
-  `,
+  template: ``,
   styles: [
   ]
 })
 export class GraphsComponent implements OnInit {
-  nameValueList:NameValue[] = [];
   constructor() { }
 
   ngOnInit(): void {
-    let nameValue:NameValue = new NameValue("hasan");
-    nameValue.setValue(150);
-
-    this.nameValueList.push(nameValue);
   }
 
 }
--- src/public-api.ts
+++ src/public-api.ts
@@ -2,7 +2,33 @@
  * Public API Surface of graphs
  */
 
-export * from './lib/graphs.component';
 export * from './lib/graphs.module';
+
+export * from './lib/graphs.component';
 export * from './lib/components/pie-graph/pie-graph.component';
 
+export * from './lib/models/inputs/config/PieGraphConfig.model';
+export * from './lib/models/inputs/config/BaseConfig.model';
+
+export * from './lib/models/inputs/config/general/Animation.model';
+export * from './lib/models/inputs/config/general/Layout.model';
+export * from './lib/models/inputs/config/general/Legend.model';
+export * from './lib/models/inputs/config/general/Responsive.model';
+export * from './lib/models/inputs/config/general/Title.model';
+export * from './lib/models/inputs/config/general/Tooltip.model';
+
+export * from './lib/models/inputs/config/general/shared/LegendLabel.model';
+export * from './lib/models/inputs/config/general/shared/Padding.model';
+
+export * from './lib/models/inputs/data/BaseData.model';
+export * from './lib/models/inputs/data/core/NameNestedValue.model';
+export * from './lib/models/inputs/data/core/NameValue.model';
+
+export * from './lib/models/inputs/enum/Align.enum';
+export * from './lib/models/inputs/enum/BorderAlign.enum';
+export * from './lib/models/inputs/enum/Easing.enum';
+export * from './lib/models/inputs/enum/FontStyle.enum';
+export * from './lib/models/inputs/enum/Mode.enum';
+export * from './lib/models/inputs/enum/Position.enum';
+export * from './lib/models/inputs/enum/TooltipAlign.enum';
+export * from './lib/models/inputs/enum/TooltipPosition.enum';
